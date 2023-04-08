a31f0a3ae276cb284abcd4a5e2163efc389ed532
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-04-08 16:01:17 -0700

    add the addSkills function to NewUserPage to post skills and desires to userskills table

--- src/pages/NewUserPage/NewUserPage.jsx
+++ src/pages/NewUserPage/NewUserPage.jsx
@@ -8,9 +8,9 @@ export default function NewUserPage({ setUser, setLoggedIn }) {
 const navigate = useNavigate();
 
 const api = process.env.REACT_APP_API_URL;
-const hereKey = process.env.HERE_API_KEY;
+// const hereKey = process.env.HERE_API_KEY;
 // console.log(process.env.HERE_API_KEY);
-console.log(`here key: ${hereKey}`)
+// console.log(`here key: ${hereKey}`)
 
 async function createNewUser(e) {
     e.preventDefault();
@@ -21,38 +21,44 @@ async function createNewUser(e) {
     const password = e.target.password.value;
     const password_confirm = e.target.password_confirm.value;
     const image_url = e.target.image.value;
-    const house = e.target.house.value;
-    const street = e.target.street.value;
+    const home = e.target.home.value;
     const city = e.target.city.value;
     const province = e.target.province.value;
-    const country = e.target.country.value;
-    const address = `${house} ${street} ${city} ${province} ${country}`;
+    const address = `${home} ${city} ${province}`;
     const addressRequest = address.replaceAll(",", ' ').replaceAll(" ", '+').replaceAll(".", '+');
     const coords = await getNewUserGeo(addressRequest); // wait for the coordinates
     const status = 'active';
     const about = e.target.about.value;
-    axios.post(`${api}/users/newuser`, {
-            user_id: user_id,
-            first_name: first_name,
-            last_name: last_name,
-            email: email,
-            password: password,
-            image_url: image_url,
-            status: status,
-            coords: coords,
-            about: about,
-            address: address,
-    })
-    .then((res) => {
+    const offers = e.target.offers.value;
+    const offersSplit = offers.split(',');
+    const offersArray = offersSplit.map(offer => offer.trim(' '));
+    const addOffers = await addSkills(offersArray, user_id, true);
+    const desires = e.target.desires.value;
+    const desiresSplit = desires.split(',');
+    const desiresArray = desiresSplit.map(desire => desire.trim(' '));
+    const addDesires = await addSkills(desiresArray, user_id, false);
+    try {
+        const response = await Promise.all([
+            axios.post(`${api}/users/newuser`, {
+                user_id: user_id,
+                first_name: first_name,
+                last_name: last_name,
+                email: email,
+                password: password,
+                image_url: image_url,
+                status: status,
+                coords: coords,
+                about: about,
+                address: address,
+            })
+        ]);
         setLoggedIn(true);
-        setUser(res.data);
+        setUser(response[0].data);
         navigate('/neighbors');
-    })
-    .catch((err) => {
+    } catch(err) {
         console.log('Error creating new user: ', err);
-    });
+    }
 }
-
 
 
 //api call to return lat long from address
@@ -67,6 +73,21 @@ async function getNewUserGeo(addressRequest) {
         // return `${res.data.items[0].position.lng},${res.data.items[0].position.lat}`;
     } catch(err) {
         console.log('Error returning lat long from api ', err)
+    }
+}
+
+async function addSkills(arr, id, which) {
+    try {
+        const response = await Promise.all(
+            arr.map(item => axios.post(`${api}/userskills`, {
+                user_id: id,
+                skill: item,
+                offer: which,
+            }))
+        );
+        return response;
+    } catch(err) {
+        console.log('Error adding skills: ', err);
     }
 }
 
@@ -89,13 +110,27 @@ async function getNewUserGeo(addressRequest) {
                 <label className="new__label">Password
                     <input type="password" className="new__input" name="password" placeholder="Password"/>
                 </label>
-                <label className="new__label">Confirm Password
+                <label className="new__label">Confirm
                     <input type="password" className="new__input" name="password_confirm" placeholder="Password again"/>
                 </label>
-                <label className="new__label">Address
+                <label className="new__label">Home Address
+                    <input type="text" className="new__input" name="home" placeholder="123 Main St"/>
+                </label>
+                <label className="new__label">City
+                    <input type="text" className="new__input" name="city" placeholder="Any Town"/>
+                </label>
+                <label className="new__label">State/Province
+                    <input type="text" className="new__input" name="province" placeholder="British Columbia"/>
+                </label>
+
+
+                {/* <label className="new__label">Address
                     <input type="text" className="new__input" name="address" placeholder="123 Main St, Any Town, BC, Canada"/>
                 </label>
                 <p className="new__desc">Please include house number, city, and state/province</p>
+                 */}
+                
+                
                 {/* <label className="new__label">House Number
                     <input type="text" className="new__input" name="house" />
                 </label>
@@ -115,18 +150,18 @@ async function getNewUserGeo(addressRequest) {
 
                 <div className="new__personalize">
                 <label className="new__label">Tell your neighbors about yourself
-                    <textarea className="new__input textarea" name="about" rows="3" maxLength={300} resize="none" placeholder="Feel free to describe your interests here, and why you're excited to connect with your fellow neighbors."/>
+                    <textarea className="new__input textarea" name="about" rows="3" maxLength={300} placeholder="Feel free to describe your interests here, and why you're excited to connect with your fellow neighbors."/>
                 </label>
                 <label className="new__label">Skills you can offer
-                    <input type="text" className="new__input" name="skills" placeholder="ie Gardening, Landscaping, Construction"/>
+                    <input type="text" className="new__input" name="offers" placeholder="ie Gardening, Landscaping, Construction"/>
                 </label>
                 <p className="new__desc">One or two words for each offering, separated by commas</p>
                 <label className="new__label">What you would like to barter for
-                    <input type="text" className="new__input" name="skills" placeholder="ie Cooking, Running Errands, Cat Sitting"/>
+                    <input type="text" className="new__input" name="desires" placeholder="ie Cooking, Running Errands, Cat Sitting"/>
                 </label>
                 <p className="new__desc">One or two words for each offering, separated by commas</p>
                 <label className="new__label">Profile Picture (url only)
-                    <input type="text" className="new__input" name="image" placeholder="https://www.awebsite.com/images/myprofilepicure.png"/>
+                    <input type="text" className="new__input" name="image" placeholder="https://picsum.photos/200/300?grayscale" value="https://picsum.photos/seed/picsum/300/300"/>
                 </label>
                 <button className="new__btn">Start Meeting Your Neighbors</button>
                 </div>
