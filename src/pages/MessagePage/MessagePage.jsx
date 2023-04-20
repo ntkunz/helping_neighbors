61678e5af652aa127f4bd81cd222bf781aacb7b1
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-04-20 08:54:24 -0700

    format code layout

--- src/pages/MessagePage/MessagePage.jsx
+++ src/pages/MessagePage/MessagePage.jsx
@@ -22,7 +22,9 @@ export default function Message({ user, neighbors }) {
 		//only retrieve messages once receiver is set to avoid 400 error
 		if (receiver.user_id) {
 			getMessages(user.user_id, receiver.user_id);
-			const messageInt = setInterval(()=>{getMessages(user.user_id, receiver.user_id)}, 2000);
+			const messageInt = setInterval(() => {
+				getMessages(user.user_id, receiver.user_id);
+			}, 2000);
 			return () => {
 				clearInterval(messageInt);
 			};
