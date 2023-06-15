3844f061ee11778a96b9668e39aa7190044948e3
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-06-15 15:33:14 -0700

    udpate sendRequest.jsx util file to accomidate new verifyUser route for refactoring/cleanup

--- src/utils/sendRequest.jsx
+++ src/utils/sendRequest.jsx
@@ -10,11 +10,6 @@ const api = process.env.REACT_APP_API_URL;
  * Sends a GET request to verify the user's token and return the user's data.
  * @return {Promise<object>} The user's data or an error object.
  */
-
-/**
- * Sends a GET request to verify the user's token and return the user's data.
- * @return {Promise<object>} The user's data or an error object.
- */
 export default async function sendRequest() {
 	// Get the user's token from local storage
 	const token = localStorage.getItem("token");
@@ -33,9 +28,10 @@ export default async function sendRequest() {
 			});
 
 			// Handle the response
-			if (response.data[0] && response.data[0].email) {
+			if (response.data && response.data.email) {
 				// If the response data is valid, return it
-				return await response.data[0];
+
+				return await response.data;
 			} else {
 				// If the response data is invalid, throw an error
 				throw new Error("Invalid response data");
@@ -47,35 +43,3 @@ export default async function sendRequest() {
 	}
 }
 
-// export default async function sendRequest() {
-// 	// Get the user's token from local storage
-// 	const token = localStorage.getItem("token");
-
-// 	// If the token is not found, return an error object
-// 	if (!token) {
-// 		return { error: "User not authenticated" };
-// 	} else {
-// 		try {
-// 			// Send the GET request to verify the token
-// 			const response = await axios.get(`${api}/users/verify`, {
-// 				headers: {
-// 					"Content-Type": "application/json",
-// 					Authorization: `Bearer ${token}`,
-// 				},
-// 			});
-
-// 			// Handle the response
-// 			if (response.data[0] && response.data[0].email) {
-// 				// If the response data is valid, return it
-// 				return await response.data[0];
-// 			} else {
-// 				// If the response data is invalid, return an error object
-// 				return { error: "Invalid response data" };
-// 			}
-// 		} catch (error) {
-// 			// Handle the error
-// 			console.log("token validation error");
-// 			return { error: "Token validation error" };
-// 		}
-// 	}
-// }
