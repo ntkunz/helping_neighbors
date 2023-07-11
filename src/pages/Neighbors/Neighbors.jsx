4164bbbb7b43a04560e8eb64c1e4db952cc11c61
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-11 05:19:29 -0700

    add early return on Messagers Page if no neighbors

--- src/pages/Neighbors/Neighbors.jsx
+++ src/pages/Neighbors/Neighbors.jsx
@@ -46,32 +46,3 @@ export default function Neighbors({ neighbors, user }) {
 		</div>
 	);
 }
-
-// return (
-// 	<div className="neighbors__container">
-// 		<h1 className="login__title">
-// 			Welcome {user.first_name}, click a neighbor's card to message them and
-// 			arrange a barter
-// 		</h1>
-
-// 		<div className="neighbors">
-// 			{neighbors.length > 0 ? (
-// 				neighbors.map((neighbor) => (
-// 					<Link
-// 						className="neighbor__link"
-// 						key={neighbor.user_id}
-// 						to={`/neighbor/${neighbor.user_id}`}
-// 					>
-// 						<Neighbor key={neighbor.user_id} neighbor={neighbor} />
-// 					</Link>
-// 				))
-// 			) : (
-// 				<h2 className="neighbors__empty">
-// 					There are no neighbors near you using the app. Please spread the
-// 					word!
-// 				</h2>
-// 			)}
-// 		</div>
-// 	</div>
-// );
-// }
